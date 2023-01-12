5fe277b3904966e5bdcb14cabaa7d395edda799f
Woodson Juste <j.woodson@hotmail.fr>
2023-01-12 11:25:56 +0100

    String + docs exo

--- javaString/src/fr/dawan/javastring/Application.java
+++ javaString/src/fr/dawan/javastring/Application.java
@@ -17,6 +17,37 @@ public class Application {
 		System.out.println(s3 == s4); //false 
 		
 		System.out.println(s1.equals(s3));  //true
+		
+		String str ="test";
+		System.out.println(str.charAt(2)); // s
+		
+		//Un objet de type String est immuable   
+		str = str.concat("e");   //"test" + "e"
+		System.out.println(str);  //teste
+		
+		//[0, 3[  ... tes
+		System.out.println("substring(0, 3) : " + str.substring(0, 3)); //tes
+		System.out.println("substring(2) : " + str.substring(2)); //ste
+		
+		//StringBuilder : Mutable (Muable)
+		StringBuilder stb2 = new StringBuilder("test");
+		StringBuilder stb3 = new StringBuilder("test");
+		System.out.println("Comparaison de reference StringBuilder : " + (stb2 == stb3));  //false
+		
+		System.out.println(stb2.append("e"));
+		
+		System.out.println("6" + 4 + 5);  //645
+		System.out.println(4 + 5 + "7");  //97
+		System.out.println("6" + (5 - 4));  // *, /, %, +, -
+		
+		
+		int x = 5 + 'A';  //Code ASCII de A est égale à 65
+		System.out.println(x);
+		
+		char x1 = 5 + 'A';  //70 ==> 'F'
+		System.out.println(x1);
+		
+
 
 	}
 
